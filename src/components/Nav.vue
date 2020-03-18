bee67b2514d153ccb0feadf764927fa6834583a9
xihaoshangdi <xihaoshangdi@163.com>
2020-03-18 18:18:34 +0800

    添加了SVG类型的Icon 添加了SVG的导入器 svg-sprite-loader 和配置 新建了SVG的自动导入函数

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,41 @@
 <template>
     <div>
-        <router-link to="/money">money</router-link>|
-        <router-link to="/labels">labels</router-link>|
-        <router-link to="/statistics">statistics</router-link>|
+        <router-link to="/money">
+            <svg>
+                <use xlink:href="#money"/>
+            </svg>
+            money
+        </router-link>
+        |
+        <router-link to="/labels">
+            <svg>
+                <use xlink:href="#labels"/>
+            </svg>
+            labels
+        </router-link>
+        |
+        <router-link to="/statistics">
+            <svg>
+                <use xlink:href="#statistics"/>
+            </svg>
+            statistics
+        </router-link>
+        |
     </div>
 </template>
 
 <script lang="ts">
+  const requireAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().map(requireContext);
+  try {
+    requireAll(require.context("../assets/icons", false, /\.svg$/));
+  } catch (e) {
+    console.log(e);
+  }
   export default {
-    name: "Nav"
+    name: "Nav",
+    data() {
+      return {};
+    }
   };
 </script>
 
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,34 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const path =require('path');
+
 module.exports = {
-  lintOnSave: false
-}
+    lintOnSave: false,
+    chainWebpack: config => {
+        const svgRule = config.module.rule("svg");
+        svgRule.uses.clear();
+        svgRule
+            .use("svg-sprite-loader")
+            .loader("svg-sprite-loader")
+            .options({
+                symbolId: "[name]",
+                include: ["./src/assets/icons"]
+            });
+    }
+};
+
+// module.exports = {
+//
+//     chainWebpack: config => {
+//         //path.resolve() 方法将路径或路径片段的序列解析为绝对路径。
+//         const dir = path.resolve(__dirname, 'src/assets/icons')//获取icons的路径
+//
+//         config.module
+//             .rule('svg-sprite')
+//             .test(/\.svg$/)
+//             .include.add(dir).end()
+//             .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false});
+//       // eslint-disable-next-line @typescript-eslint/no-var-requires
+//         config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}]);
+//         config.module.rule('svg').exclude.add(dir)
+//     }
+// };
