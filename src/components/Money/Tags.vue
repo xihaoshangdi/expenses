ce52835a2261f7b9fd4044a2d489cc1d0f5b27f0
xihaoshangdi <xihaoshangdi@163.com>
2020-03-20 15:55:01 +0800

    更新了Tags的样式

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -7,42 +7,46 @@
                     :key="item"
                     @click="toggle(item)"
                     :class="{'selected':selectedTags.indexOf(item)>=0}"
-            >{{item}}</li>
+            >{{item}}
+            </li>
         </ul>
-        <button class="tagBtn" @click="create">添加标签</button>
+        <button class="tagBtn" @click="create">Add</button>
     </div>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-  import {Component, Prop,Watch} from "vue-property-decorator";
+  import Vue from "vue";
+  import {Component, Prop, Watch} from "vue-property-decorator";
 
   @Component
   export default class Tags extends Vue {
     selectedTags: string[] = [];
-    @Prop(Array) dataSource: string[]|undefined;
-    @Watch('selectedTags', { immediate: false})
+    @Prop(Array) dataSource: string[] | undefined;
+
+    @Watch("selectedTags", {immediate: false})
     onTagChanged(val: string[]) {
-      this.$emit('update:value',val);
+      this.$emit("update:value", val);
     }
+
     toggle(tag: string) {
       const index = this.selectedTags.indexOf(tag);
       if (index < 0) {
         this.selectedTags.push(tag);
-      }else{
+      } else {
         this.selectedTags.splice(index, 1);
       }
       // this.$emit('xxx',this.selectedTags);
     }
-    create(){
-      const name = window.prompt('请输入标签名');
-      if (name === '') {
-        window.alert('标签名不能为空');
-      } else if (name===null){
-        return
+
+    create() {
+      const name = window.prompt("请输入标签名");
+      if (name === "") {
+        window.alert("标签名不能为空");
+      } else if (name === null) {
+        return;
       } else if (this.dataSource) {
-        console.log('here');
-        this.$emit('update:dataSource',
+        console.log("here");
+        this.$emit("update:dataSource",
           [...this.dataSource, name]);
       }
     }
@@ -51,16 +55,21 @@
 
 <style lang="scss" scoped>
     @import "~@/assets/styles/global.scss";
-    .box{
+
+    .box {
         @extend %informationBox
     }
+
     .tagList {
-        width: 60vw;
+        width: 70vw;
         margin-left: 15px;
         display: flex;
         flex-direction: row;
+        overflow-x: auto;
+        position: relative;
 
         > li {
+
             background: $rgba;
             $h: 24px;
             height: $h;
@@ -68,17 +77,20 @@
             border-radius: $h/2;
             padding: 0 10px;
             margin-right: 10px;
+            flex: none;
 
-            &.selected{
+            &.selected {
                 background: $tag-choose-bg;
                 color: $tag-choose-color;
             }
         }
     }
-    .tagBtn{
-        text-wrap: none;
-        background: transparent;
+
+    .tagBtn {
+        right: 15px;
+        position: absolute;
         border: none;
+        background: transparent;
     }
 
 </style>
