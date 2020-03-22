679e108a714f242171e2f6123fd6f5666aef6f88
xihaoshangdi <xihaoshangdi@163.com>
2020-03-22 21:23:24 +0800

    更新Edit组件

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -7,10 +7,25 @@
 <script lang="ts">
   import Vue from "vue";
   import {Component} from "vue-property-decorator";
+  import labelBar from "@/models/labelModel";
 
   @Component
   export default class EditLabel extends Vue {
+    label={};
 
+    created(): void {
+
+      const id = this.$route.params.id;
+      labelBar.extract();
+      console.log(labelBar.labelsList);
+      this.label = labelBar.labelsList.filter(item => item.id === id)[0];
+      if (this.label){
+        console.log(this.label);
+      }else{
+        this.$router.replace('/404');
+      }
+
+    }
   }
 </script>
 
