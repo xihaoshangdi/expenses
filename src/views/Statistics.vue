268728cc9f27ef215a573a87df942dc30628ee40
xihaoshangdi <xihaoshangdi@163.com>
2020-03-27 23:44:18 +0800

    完成月份功能

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -15,7 +15,7 @@
                         v-for="(item,index) of groupList"
                         :key="index"
                 >
-                    <h3 class="title">{{timeFormat(item.title)}}<span>￥{{item.total}}</span></h3>
+                    <h3 class="title">{{timeFormat(item.title)}}<span>￥{{item.total||0}}</span></h3>
                     <div class="record-box">
                         <ol>
                             <li v-for="(i,index) of item.recordList"
@@ -24,7 +24,7 @@
                             >
                                 <span>{{labelString(i.tags)}}</span>
                                 <span class="headline">{{i.headline}}</span>
-                                <span>￥{{i.amount}}</span>
+                                <span>￥{{i.amount||0}}</span>
                             </li>
                         </ol>
                     </div>
@@ -51,15 +51,23 @@
     total: number;
     recordList: RecordBar[];
   }[];
+  type Interval = {
+    type: week | day | month;
+  }
   @Component({
     components: {Tabs},
   })
 
   export default class Statistics extends Vue {
     type = "-";
-    interval = "day";
+    interval='week';
     intervalList = intervalList;
     recordTypeList = recordTypeList;
+
+    get region(){
+      return {type:this.interval} as Interval
+    }
+
 
     labelString(tags: string[]) {
       return tags.length === 0 ? "无" : tags.join(",");
@@ -68,21 +76,37 @@
     timeFormat(value: string) {
       const date = value.split("T")[0];
       const today = dayjs();
-      if (today.isSame(date, "day")) {
-        return "今天";
-      } else if (today.subtract(1, "day").isSame(date, "day")) {
-        return "昨天";
-      } else if (today.subtract(2, "day").isSame(date, "day")) {
-        return "前天";
-      } else if (today.isSame(date, "year")) {
-        return dayjs(date).format("M月D日"); // '25/01/2019'
-      } else {
-        return dayjs(date).format("YY年M月D日");
+      if (this.region.type==='day'){
+        if (today.isSame(date, "day")) {
+          return "今天";
+        } else if (today.subtract(1, "day").isSame(date, "day")) {
+          return "昨天";
+        } else if (today.subtract(2, "day").isSame(date, "day")) {
+          return "前天";
+        } else if (today.isSame(date, "year")) {
+          return dayjs(date).format("M月D日"); // '25/01/2019'
+        } else {
+          return dayjs(date).format("YY年M月D日");
+        }
+      }else if(this.region.type==='week'){
+        if (today.isSame(date, "week")) {
+          return "本周";
+        } else{
+          if (today.isSame(date, "year")){
+            const num=dayjs(date).day();
+            const weekStart=dayjs(date).subtract(num,'day');
+            const weekEnd=dayjs(date).add(7-num,'day');
+            console.log(weekStart,weekEnd);
+            return dayjs(weekStart).format("M月D日")+'--'+dayjs(weekEnd).format("M月D日");
+          }
+
+        }
       }
+
     }
 
     get groupList() {
-      const dataList: RecordBar[] =Clone(this.$store.state.recordData)
+      const dataList: RecordBar[] = Clone(this.$store.state.recordData)
         .filter((item: RecordBar) => item.type === this.type);
       dataList.sort((a, b) => {
         return dayjs(b.date).valueOf() - dayjs(a.date).valueOf();
@@ -92,10 +116,11 @@
         total: parseFloat(dataList[0].amount),
         recordList: [dataList[0]]
       }];
+
       for (let i = 1; i < dataList.length; i++) {
         const current = dataList[i];
         const last = result[result.length - 1];
-        if (dayjs(current.date).isSame(last.title, "day")) {
+        if (dayjs(current.date).isSame(last.title, this.region.type)) {
           last.total += parseFloat(current.amount);
           last.recordList.push(current);
         } else {
